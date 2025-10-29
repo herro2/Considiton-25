15ee5f0b708ec3d5e147b75ffd089c866515ea1b
Fredrik Karlsson <fredrikkarlsson15@gmail.com>
2025-10-29 15:04:36 +0100

    Added dto, updated usage.

--- Considition2025_CsharpStarterKit/Dtos/Request/CustomerRecommendationDto.cs
+++ Considition2025_CsharpStarterKit/Dtos/Request/CustomerRecommendationDto.cs
@@ -3,5 +3,11 @@
 public record CustomerRecommendationDto
 {
     public string? CustomerId { get; set; }
-    public List<string> ChargingRecommendations { get; set; } = [];
+    public List<ChargingRecommendationDto> ChargingRecommendations { get; set; } = [];
+}
+
+public record ChargingRecommendationDto
+{
+    public string NodeId { get; set; }
+    public float ChargeTo { get; set; }
 }
